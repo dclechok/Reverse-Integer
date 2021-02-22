07f8d2ef2691f138fafd46968d9f136a53fa7201
Daniel C Lechok <dclechok@gmail.com>
2021-02-22 23:33:50 +0000

    Cleaned up some code.  Added a few comments.

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-/**
+/*
+Reverse integer.  If signed, remain signed.
 Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
- */
-
+*/
 var reverse = function(x) {
-	let isSigned = false
 	let numArray = 0
-	if(x < 0){
-		isSigned = true
-	}
-	if(isSigned){numArray = Number(x.toString().replace('-', '').split('').reverse().join('')) * (-1)}
+	if(x < 0){numArray = Number(x.toString().replace('-', '').split('').reverse().join('')) * (-1)}
 	else{numArray = Number(x.toString().replace('-', '').split('').reverse().join(''))}
 
 	if(numArray >= 2**31 || numArray <= (2**31) * (-1)){return 0} //numArray bigger than 32-bit 
@@ -16,4 +12,4 @@ var reverse = function(x) {
 	else{return numArray}
 };
 
-reverse(1534236469)
+reverse(12)
