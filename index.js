8c164cc2c414b36eac7664df9cc918a878c93134
Daniel C Lechok <34754421+dclechok@users.noreply.github.com>
2021-02-22 18:39:45 -0500

    Update index.js
    Some additional comments.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /*
-Reverse integer.  If signed, remain signed.
+Reverse Integer.  Practice problem #7 on leetcode.com
 Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 */
+
 var reverse = function(x) {
 	let numArray = 0
 	if(x < 0){numArray = Number(x.toString().replace('-', '').split('').reverse().join('')) * (-1)}
@@ -12,4 +13,4 @@ var reverse = function(x) {
 	else{return numArray}
 };
 
-reverse(12)
+reverse(12) //for debug - number to reverse
